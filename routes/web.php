49e77d99daae6aacc35c94d5f04b0126e88617cd
Tomas Marlein <tomas.marlein@gmail.com>
2020-01-22 15:24:06 +0100

    database

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,15 @@ Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::view('/', 'landingpage');
-Route::view('/home', 'home');
-Route::view('verantwoordelijke', 'Admin\VerantwoordelijkeController@index');
-Route::view('verenigingen', 'Admin\VerenigingController@index');
-
-
-
-Route::get('qryVerenigingen','Admin\VerenigingController@qryVerenigingen');
-Route::resource('verenigingen', 'Admin\VerenigingController');
-
-    // Route::get('verantwoordelijke', '');
-
-Route::get('qryVerant', 'Admin\VerantwoordelijkeController@qryVerantwoordelijke');
-Route::resource('verantwoordelijke', 'Admin\VerantwoordelijkeController');
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::view('/home', 'home');
+    Route::view('verantwoordelijke', 'Admin\VerantwoordelijkeController@index');
+    Route::view('verenigingen', 'Admin\VerenigingController@index');
 
+    Route::get('qryVerenigingen','Admin\VerenigingController@qryVerenigingen');
+    Route::resource('verenigingen', 'Admin\VerenigingController');
 
+    Route::get('qryVerant', 'Admin\VerantwoordelijkeController@qryVerantwoordelijke');
+    Route::resource('verantwoordelijke', 'Admin\VerantwoordelijkeController');
 });
