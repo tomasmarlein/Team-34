f06ff534248d75ed4734ed1927a4ed312c36fbe1
LukaKolb <56261305+LukaKolb@users.noreply.github.com>
2020-01-28 13:27:01 +0100

    Bg fix + event dp

--- resources/views/admin/adminpanel.blade.php
+++ resources/views/admin/adminpanel.blade.php
@@ -3,12 +3,12 @@
 @section('css_after')
     <style>
         body, html {
-            height: 100%;
+
             background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("assets/images/hero.jfif") ;
             background-repeat: no-repeat;
             background-size: cover;
             background-position: center;
-            height: 100%;
+            background-color: #443339 ;
         }
 
 
@@ -44,7 +44,9 @@
             color: black;
             text-underline: none;
         }
-
+        .btn-group{
+            margin-bottom: 5%;
+        }
 
     </style>
 @endsection
@@ -54,20 +56,20 @@
 <div>
     <div class="jumbotron">
         <h1 class="display-4">Adminpanel</h1>
-        <p class="lead">Welkom in het tijdsregistratiesysteem van Keizer Karel Olen</p>
+        <p class="lead">Welkom {{ Auth::user()->naam }} in het tijdsregistratiesysteem van Keizer Karel Olen</p>
         <hr class="my-4">
-        <p>Beheer hier evenementen, verenigingen en vrijwilligers.</p>
-        <p>geselecteerd evenement</p>
+        <p>Beheer hier evenementen, verenigingen, vrijwilligers en de tijdsregistratie voor evenementen van Keizer Karel Olen.</p>
+
 
 
         <div class="btn-group">
+
             <button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 alle evenementen
             </button>
             <div class="dropdown-menu">
                 @foreach($evenementen as $evenement)
                     <option value="{{ $evenement->id }}" class="dropdown-item" href="#">{{ $evenement->naam }}</option>
-                <a class="dropdown-item" href="#">Another action</a>
                 @endforeach
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#">allemaal</a>
@@ -143,23 +145,7 @@
             </div>
 
         @endif
-
-
-
-
-
-
-
 </div>
-        @if(auth()->user()->rolID=1)
-            <a class="btn btn-primary btn-lg" href="/admin/evenementen" role="button">Evenementen</a>
-            <a class="btn btn-primary btn-lg" href="/admin/verantwoordelijke" role="button">verantwoordelijke</a>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Kernleden</a>
-            <a class="btn btn-primary btn-lg" href="/inaanvraag" role="button">In aanvraag</a>
-        @endif
-            <a class="btn btn-primary btn-lg" href="/admin/verenigingen" role="button">Verenigingen</a>
-            <a class="btn btn-primary btn-lg" href="/admin/vrijwilligers" role="button">Vrijwilligers</a>
-
     </div>
 </div>
 @endsection
