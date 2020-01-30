41c3dc2366b0b529f92eba27a5161211ea0cbf50
LukaKolb <56261305+LukaKolb@users.noreply.github.com>
2020-01-30 14:49:15 +0100

    vernantwoordelijke pagina's

--- resources/views/verantwoordelijke/vereniging.blade.php
+++ resources/views/verantwoordelijke/vereniging.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.template')
 
-@section('title', $vereniging->naam)
+@section('title', '$vereniging->naam')
 
 @section('main')
-    <h1 style="float:left">{{$vereniging->naam}}</h1>
-
-
+    <h1 style="float:left"></h1>
 
 @endsection
 
